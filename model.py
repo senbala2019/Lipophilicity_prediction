daf0ba5156d1767406ca276b2d508cb461cdd3db
senbala2019 <78355793+senbala2019@users.noreply.github.com>
2022-05-19 09:37:53 +0800

    Update model.py

--- model.py
+++ model.py
@@ -2,8 +2,7 @@ import torch
 import torch.nn.functional as F 
 from torch.nn import Linear, BatchNorm1d, ModuleList
 from torch_geometric.nn import TransformerConv, TopKPooling 
-from torch_geometric.nn import global_mean_pool as gap, global_max_pool as gmp
-torch.manual_seed(42)
+from torch_geometric.nn import global_mean_pool, global_max_pool
 
 class GNN(torch.nn.Module):
     def __init__(self, feature_size, model_params):
